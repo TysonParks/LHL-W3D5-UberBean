e2e1d6523220ce08b3f8f9df55a18686215b5b7f
tysonparks <typarks@gmail.com>
2018-03-02 17:41:48 -0800

    Initial Commit

---
+++ UberBean/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UberBean
+//
+//  Created by Tyson Parks on 3/2/18.
+//  Copyright © 2018 Tyson Parks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UberBean/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UberBean
+//
+//  Created by Tyson Parks on 3/2/18.
+//  Copyright © 2018 Tyson Parks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
