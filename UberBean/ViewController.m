15ea7128b4ad2fa5a76afda1327fc0df1394cc98
tysonparks <typarks@gmail.com>
2018-03-02 18:58:21 -0800

    Set up things properly and cinfirmed call of didUpdatLocations, but only in simulator, not on phone

--- UberBean/ViewController.m
+++ UberBean/ViewController.m
@@ -23,21 +23,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    
     self.locationManager = [[CLLocationManager alloc]init];
-    [self.
-     locationManager requestWhenInUseAuthorization];
+    [self.locationManager requestWhenInUseAuthorization];
+    self.locationManager.delegate = self;
+    
     self.mapView.showsUserLocation = YES;
+
     
     
 }
 
 
+-(void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status {
+    if (status == kCLAuthorizationStatusAuthorizedWhenInUse) {
+        [self.locationManager requestLocation];
+    }
+}
+
 -(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations{
-    
+    NSLog(@"didUpdateLocations: %@", locations);
 }
 
 -(void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
-    
+    NSLog(@"didFailWithError: %@", error);
 }
 
 
